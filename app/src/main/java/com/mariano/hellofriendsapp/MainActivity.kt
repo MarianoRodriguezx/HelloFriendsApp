28b724170f826ec0ee00137c0655cafa0154219f
MarianoRodriguezx <marianoroddhd@gmail.com>
2022-08-23 19:59:26 -0500

    [ ADD ] Navbar and Navigation
    Se agrega el navbar como menu asi como el navigation para trasladarse entre fragmentos

--- app/src/main/java/com/mariano/hellofriendsapp/MainActivity.kt
+++ app/src/main/java/com/mariano/hellofriendsapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.mariano.hellofriendsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.navigation.NavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mariano.hellofriendsapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigation.setOnItemSelectedListener { item ->
+            when(item.itemId){
+
+                R.id.home -> {
+                    Toast.makeText(this, "ENTRO A EL HOME", Toast.LENGTH_SHORT).show()
+                }
+            }
+            return@setOnItemSelectedListener true
+        }
     }
 }
